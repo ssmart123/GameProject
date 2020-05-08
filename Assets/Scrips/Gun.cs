b10c75172749654b4ff34cc65b0a32112deb7573
ssmart123 <63942174+ssmart123@users.noreply.github.com>
2020-05-08 18:20:58 +0900

    FPS_Game
    3인칭은 아직 어려워서 1인칭으로 개발중.
    추후 3인칭으로 변경 예정

---
+++ Assets/Scrips/Gun.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [Tooltip("총 이름")]
+    public string gunName;
+    [Tooltip("사정거리")] 
+    public float Range;
+    [Tooltip("정확도")]
+    public float Accuracy;
+    [Tooltip("연사속도")]
+    public float fireRate;
+    [Tooltip("재장전 속도")]
+    public float relodeBulletCount;
+
+
+    [Tooltip("총의 데미지")]
+    public int damage;
+
+    [Tooltip("총알 재장전 갯수")]
+    public int reloadBulletCount;
+    [Tooltip("현제 탄알집에 남았는 총알의 갯수")]
+    public int currentBulletCount;
+    [Tooltip("최대 소유 가능 총알 갯수")]
+    public int maxBulletCount;
+    [Tooltip("현재 소유하고 있는 총알 갯수")]
+    public int carryBulletCount;
+
+    [Tooltip("반동 세기")]
+    public float retroActionForce;
+    [Tooltip("정조준시 반동 세기")]
+    public float retroActionFineSightForce;
+
+    public Vector3 fineSightOriginPos;
+    public Animator anim;
+    public ParticleSystem muzzleFlesh;
+
+    public AudioClip fire_Sound; 
+
+}
---
+++ Assets/Scrips/Move.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float RunSpeed;
+    private float applySpeed;
+
+    public float JumpForce;
+
+    private bool isGround = true;
+
+    private CapsuleCollider capsuleCollider;
+    private Rigidbody Myrigid;
+
+    void Start()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        Myrigid = GetComponent<Rigidbody>();
+        applySpeed = MoveSpeed;
+    }
+
+    void Update()
+    {
+        Running();
+        IsGround();
+        Jump();
+        Moving();
+    }
+
+
+   
+    private void Running()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            applySpeed = RunSpeed;
+        }
+        else 
+        {
+            applySpeed = MoveSpeed;
+        }
+    }
+    private void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    }
+    
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Myrigid.velocity = transform.up * JumpForce;
+        }
+    }
+    private void Moving()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate((Vector3.forward).normalized * applySpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate((Vector3.back).normalized * applySpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate((Vector3.left).normalized * applySpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate((Vector3.right).normalized * applySpeed * Time.deltaTime);
+        }
+    }
+}
